d8f8550d3b7c6520837f82908fdf9c861194f305
suncak <71878724+suncak@users.noreply.github.com>
2023-07-06 19:34:25 +0530

    modified to handle zero division error and format output string

--- planting calculater.py
+++ planting calculater.py
@@ -1,14 +1,14 @@
 import PySimpleGUI as Sg
 import calculations
 
-area_input = Sg.Input(key="Area")
-hectare_button = Sg.Button("Hectare")
-acre_button = Sg.Button("Acre")
-esp_input = Sg.Input(key="Espacement")
-esp_meter_button = Sg.Button("Meter")
-esp_feet_button = Sg.Button("Feet")
-calculate_button = Sg.Button("Calculate")
-out_put = Sg.Text(size=(10, 1), key="-OUTPUT-")
+area_input = Sg.Input(size=(25,15), key="Area")
+hectare_button = Sg.Button("Hectare", size=(7,1))
+acre_button = Sg.Button("Acre", size=(7,1))
+esp_input = Sg.Input(size=(25,15), key="Espacement")
+esp_meter_button = Sg.Button("Meter", size=(7,1))
+esp_feet_button = Sg.Button("Feet", size=(7,1))
+calculate_button = Sg.Button("Calculate", size=(10,1))
+out_put = Sg.Text(size=(30, 1), key="-OUTPUT-")
 exit_button = Sg.Button("Exit")
 clear_button = Sg.Button("Clear")
 
@@ -18,7 +18,7 @@ layout = [
     [calculate_button, clear_button, out_put], [exit_button]]
 
 
-window = Sg.Window("Plantation Calculator", layout)
+window = Sg.Window("Plantation Calculator", layout, size=(400,175))
 
 
 while True:
@@ -34,24 +34,27 @@ while True:
             total_area = raw_area
         elif event == "Acre":
             total_area = float(calculations.t_area(raw_area))
-            print(total_area)
 
         raw_esp = float(values["Espacement"])
         if event == "Meter":
             esp = raw_esp
         elif event == "Feet":
             esp = raw_esp * 0.3048
-            print(esp)
 
         if event == "Calculate":
             result = calculations.planting_calculation(total_area, esp)
-            window["-OUTPUT-"].update(result, text_color="white")
+            window["-OUTPUT-"].update(f"maximum seedlings: {result}", font=(16),text_color="yellow")
 
         if event == "Clear":
             for key in values:
                 window[key]('')
+                window["-OUTPUT-"](" ")
 
     except ValueError:
-        window["-OUTPUT-"].update(value="Invalid input", text_color="red")
+        # deceiting the value error
+        window["-OUTPUT-"].update(value=" ", text_color="red")
+
+    except ZeroDivisionError:
+        window["-OUTPUT-"].update(value="Enter non-zero value", text_color="dark blue")
 
 window.close()
