7914da4bccf4444876ca01e5bf8fc383b5ea19de
suncak <71878724+suncak@users.noreply.github.com>
2023-07-14 08:21:07 +0530

    all logical errors rectified. still some issues

--- calculations.py
+++ calculations.py
@@ -5,6 +5,10 @@ def t_area(ar):
     tot_area = float(ar) * float(0.4047)
     return tot_area
 
+def t1_area(ar,au):
+    tot_area = float(ar) * float(0.4047)
+    return tot_area
+
 
 def planting_calculation(tot_area, espace):
     no_of_plants = math.ceil((tot_area * 10000) / espace ** 2)
